14aceaef0061c9a232e4609dae2e595377411082
Dreezy305 <bankoleidris@gmail.com>
2023-01-19 15:14:46 +0100

    loaded fonts, bell, favico etc

--- components/FilterOptions.tsx
+++ components/FilterOptions.tsx
@@ -1,128 +1,90 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
 import React from "react";
 
 function FilterOptions(): JSX.Element {
   return (
     <div className="">
-      <div className="flex flex-row place-self-center options w-full">
-        <div className="options_content">
-          <p className="flex flex-row items-center space-x-3 py-2 px-3">
-            <span className="me-2 font-roboto text-darkText font-medium">
-              World
-            </span>
-            <span className="">
-              <Image
-                src={"/img/chevronDown.svg"}
-                className=""
-                alt="chevron_down"
-                width={30}
-                height={30}
-              />
-            </span>
-          </p>
+      <div className="flex flex-row place-self-center options md:w-4/5 lg:w-full border">
+        <div className="cursor-pointer border-r-2 border-solid border-r-light flex flex-row justify-between items-center px-4">
+          <span className="me-2 font-roboto font-medium text-darkText">
+            World
+          </span>
+          <span className="">
+            <img src={"/img/chevronDown.svg"} className="" alt="chevron_down" />
+          </span>
         </div>
 
-        <div className="options_content">
-          <p className="flex flex-row items-center space-x-3 py-2 px-3">
-            <span className="me-2 font-roboto font-medium text-darkText">
-              Following
-            </span>
-            <span className="">
-              <Image
-                src={"/img/chevronDown.svg"}
-                className=""
-                alt="chevron_down"
-                width={30}
-                height={30}
-              />
-            </span>
-          </p>
+        <div className="cursor-pointer border-r-2 border-solid border-r-light flex flex-row justify-between items-center px-4">
+          <span className="me-2 font-roboto font-medium text-darkText">
+            Following
+          </span>
+          <span className="">
+            <img
+              src={"/img/chevronDown.svg"}
+              className=""
+              alt="chevron_down"
+              // width={30}
+              // height={30}
+            />
+          </span>
         </div>
 
-        <div className="options_content">
-          <p className="flex flex-row items-center space-x-3 py-2 px-3">
-            <span className="me-2 font-roboto font-medium text-darkText">
-              Popular
-            </span>
-            <span className="">
-              <Image
-                src={"/img/chevronDown.svg"}
-                className=""
-                alt="chevron_down"
-                width={30}
-                height={30}
-              />
-            </span>
-          </p>
+        <div className="cursor-pointer border-r-2 border-solid border-r-light flex flex-row justify-between items-center px-4">
+          <span className="me-2 font-roboto font-medium text-darkText">
+            Popular
+          </span>
+          <span className="">
+            <img src={"/img/chevronDown.svg"} className="" alt="chevron_down" />
+          </span>
         </div>
 
-        <div className="options_content">
-          <p className="flex flex-row items-center space-x-3 py-2 px-3">
-            <span className="me-2 font-roboto text-darkText font-medium">
-              Post
-            </span>
-            <span className="">
-              <Image
-                src={"/img/chevronDown.svg"}
-                className=""
-                alt="chevron_down"
-                width={30}
-                height={30}
-              />
-            </span>
-          </p>
+        <div className="cursor-pointer border-r-2 border-solid border-r-light flex flex-row justify-between items-center px-4">
+          <span className="me-2 font-roboto font-medium text-darkText">
+            Post
+          </span>
+          <span className="">
+            <img
+              src={"/img/chevronDown.svg"}
+              className=""
+              alt="chevron_down"
+              // width={30}
+              // height={30}
+            />
+          </span>
         </div>
 
-        <div className="options_content">
-          <p className="flex flex-row items-center space-x-3 py-2 px-3">
-            <span className="me-2 font-roboto font-medium text-darkText">
-              Gender
-            </span>
-            <span className="">
-              <Image
-                src={"/img/chevronDown.svg"}
-                className=""
-                alt="chevron_down"
-                width={30}
-                height={30}
-              />
-            </span>
-          </p>
+        <div className="cursor-pointer border-r-2 border-solid border-r-light flex flex-row justify-between items-center px-4">
+          <span className="me-2 font-roboto font-medium text-darkText">
+            Gender
+          </span>
+          <span className="">
+            <img
+              src={"/img/chevronDown.svg"}
+              className=""
+              alt="chevron_down"
+              // width={30}
+              // height={30}
+            />
+          </span>
         </div>
 
-        <div className="options_content">
-          <p className="flex flex-row items-center space-x-3 py-2 px-3">
-            <span className="me-2 font-roboto font-medium text-darkText">
-              Location
-            </span>
-            <span className="">
-              <Image
-                src={"/img/chevronDown.svg"}
-                className=""
-                alt="chevron_down"
-                width={30}
-                height={30}
-              />
-            </span>
-          </p>
+        <div className="cursor-pointer border-r-2 border-solid border-r-light flex flex-row justify-between items-center px-4">
+          <span className="me-2 font-roboto font-medium text-darkText">
+            Location
+          </span>
+          <span className="">
+            <img src={"/img/chevronDown.svg"} className="" alt="chevron_down" />
+          </span>
         </div>
 
-        <div
-          className="options_content"
-          style={{ borderRight: "0px solid #f5f5f5" }}
-        >
-          <p className="flex flex-row items-center space-x-3 py-2 px-3 font-medium text-darkText">
-            <span className="me-2 font-roboto">Profession</span>
-            <span className="">
-              <Image
-                src={"/img/chevronDown.svg"}
-                className=""
-                alt="chevron_down"
-                width={30}
-                height={30}
-              />
-            </span>
-          </p>
+        <div className="cursor-pointer border-r-0 border-solid border-r-light flex flex-row justify-between items-center px-4">
+          <span className="me-2 font-roboto font-medium text-darkText">
+            Profession
+          </span>
+          <span className="">
+            <img src={"/img/chevronDown.svg"} className="" alt="chevron_down" />
+          </span>
         </div>
       </div>
     </div>
